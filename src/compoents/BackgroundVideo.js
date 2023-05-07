ceaccf00a6f3990a5f7e159b10d5699b4693f280
Barcha1 <abbarcha123456@gmail.com>
2023-05-08 00:40:43 +0500

    kk

--- src/compoents/BackgroundVideo.js
+++ src/compoents/BackgroundVideo.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import sampleVideo from "../Assets/Untitled.mp4"
 
 const BackgroundVideo = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (videoRef.current) {
+        videoRef.current.play();
+      }
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <div> <video  className="background__Img" autoPlay loop muted>
-    <source src={sampleVideo} type='video/mp4' />
-    </video></div>
+    <div>
+      <video ref={videoRef} className="background__Img" loop muted>
+        <source src={sampleVideo} type='video/mp4' />
+      </video>
+    </div>
   )
 }
 
